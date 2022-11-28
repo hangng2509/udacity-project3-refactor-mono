98798246ad4fdcdd84c0d291c7d9c3de92fd7547
hangng2509 <hn192509@gmail.com>
2022-11-29 00:09:18 +0700

    Submit finally

--- kubernetes/deploy_k8s.sh
+++ kubernetes/deploy_k8s.sh
@@ -1,19 +1,24 @@
-kubectl delete deploy api-user
-kubectl delete deploy api-feed
-kubectl delete deploy reverseproxy
-kubectl delete deploy frontend
+# kubectl delete deploy api-user
+# kubectl delete deploy api-feed
+# kubectl delete deploy reverseproxy
+# kubectl delete deploy frontend
 
-# # Apply env variables and secrets
-kubectl apply -f aws-cre.yaml
-kubectl apply -f env-secret.yaml
-kubectl apply -f env-configmap.yaml
+# # # Apply env variables and secrets
+# kubectl apply -f aws-cre.yaml
+# kubectl apply -f env-secret.yaml
+# kubectl apply -f env-configmap.yaml
 
-kubectl apply -f api-feed-deployment.yaml
-kubectl apply -f api-user-deployment.yaml
-kubectl apply -f reverseproxy-deployment.yaml
-kubectl apply -f frontend-deployment.yaml
+# kubectl apply -f api-feed-deployment.yaml
+# kubectl apply -f api-user-deployment.yaml
+# kubectl apply -f reverseproxy-deployment.yaml
+# kubectl apply -f frontend-deployment.yaml
 
-kubectl apply -f api-feed-service.yaml
-kubectl apply -f api-user-service.yaml
-kubectl apply -f reverseproxy-service.yaml
-kubectl apply -f frontend-service.yaml
+# kubectl apply -f api-feed-service.yaml
+# kubectl apply -f api-user-service.yaml
+# kubectl apply -f reverseproxy-service.yaml
+# kubectl apply -f frontend-service.yaml
+
+kubectl apply -f api-feed-hpa.yaml
+kubectl apply -f api-user-hpa.yaml
+kubectl apply -f reverseproxy-hpa.yaml
+kubectl apply -f frontend-hpa.yaml
--- udagram-api-user/src/config/config.ts
+++ udagram-api-user/src/config/config.ts
@@ -6,7 +6,7 @@ export const config = {
   dialect: "postgres",
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
-  aws_media_bucket: "mybucket-067328141418-dev",
+  aws_media_bucket: process.env.AWS_BUCKET,
   url: process.env.URL,
   jwt: {
     secret: process.env.JWT_SECRET,
