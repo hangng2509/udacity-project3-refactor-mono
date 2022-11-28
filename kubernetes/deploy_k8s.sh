5ba63f822c025ed0fe3f814e39c115467bebcaa0
hangng2509 <hn192509@gmail.com>
2022-11-28 21:34:00 +0700

    Update 11

--- kubernetes/deploy_k8s.sh
+++ kubernetes/deploy_k8s.sh
@@ -1,17 +1,17 @@
-# kubectl delete deploy api-user
-# kubectl delete deploy api-feed
-# kubectl delete deploy reverseproxy
-# kubectl delete deploy frontend
+kubectl delete deploy api-user
+kubectl delete deploy api-feed
+kubectl delete deploy reverseproxy
+kubectl delete deploy frontend
 
 # # Apply env variables and secrets
-# kubectl apply -f aws-cre.yaml
-# kubectl apply -f env-secret.yaml
-# kubectl apply -f env-configmap.yaml
+kubectl apply -f aws-cre.yaml
+kubectl apply -f env-secret.yaml
+kubectl apply -f env-configmap.yaml
 
-# kubectl apply -f api-feed-deployment.yaml
-# kubectl apply -f api-user-deployment.yaml
-# kubectl apply -f reverseproxy-deployment.yaml
-# kubectl apply -f frontend-deployment.yaml
+kubectl apply -f api-feed-deployment.yaml
+kubectl apply -f api-user-deployment.yaml
+kubectl apply -f reverseproxy-deployment.yaml
+kubectl apply -f frontend-deployment.yaml
 
 kubectl apply -f api-feed-service.yaml
 kubectl apply -f api-user-service.yaml
